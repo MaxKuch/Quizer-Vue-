ffd6712f8ef18a17044ef4d9525487d92cf85b49
Max <narrm4517@gmail.com>
2020-08-16 15:55:48 +0300

    added vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  css: {
-    loaderOptions: {
-      // `scss` syntax requires an semicolon at the end of a statement, while `sass` syntax requires none
-      // in that case, we can target the `scss` syntax separately using the `scss` option
-      scss: {
-        prependData: `@import "~@/assets/_variables.scss";`
-      },
+  "css": {
+    "loaderOptions": {
+      "scss": {
+        "prependData": "@import \"~@/assets/_variables.scss\";"
+      }
     }
-  }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
